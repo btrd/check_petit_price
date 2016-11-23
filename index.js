1db2fe75c94c06f8ae9c58376a897343a7b4cce0
Jean Bertrand <jeanbertrand.contact@gmail.com>
2016-11-23 16:09:11 +0100

    Set alarm for sunday

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var request = require('request-promise');
 var config = require('dotenv').config();
 
-var sms_send = false;
+var sms_send = 0;
 
 function send_sms(proposal) {
   var url = 'https://smsapi.free-mobile.fr/sendmsg?user=' + process.env.FREE_MOBILE_USER + '&pass=' + process.env.FREE_MOBILE_PASSWORD + '&msg=';
@@ -9,7 +9,7 @@ function send_sms(proposal) {
   
   request(url)
   .then((res) => {
-    sms_send = true;
+    sms_send += 1;
     console.log('Proposal send by sms', proposal)
   })
   .catch((err) => {
@@ -17,11 +17,11 @@ function send_sms(proposal) {
   });
 }
 
-function check_price_for(date) {
-  request({ uri: 'http://calendar.voyages-sncf.com/cdpPetitsPrix/api/odm/v1/FRLLE/FRAEG/YOUNG?nbPassengers=1', json: true })
+function check_price_for(date, start, end) {
+  request({ uri: 'http://calendar.voyages-sncf.com/cdpPetitsPrix/api/odm/v1/' + start + '/' + end + '/YOUNG?nbPassengers=1', json: true })
   .then((prices) => {
     var proposal = prices.proposalsByDay.filter((elt) => { return elt.day === date })[0];
-
+    console.log(new Date(), proposal);
     if(proposal.proposalsNumber > 0) {
       send_sms(proposal);
     }
@@ -32,6 +32,11 @@ function check_price_for(date) {
 }
 
 setInterval(() => {
-  if (sms_send) { process.exit(); }
-  check_price_for('2016-11-25');
+  main();
 }, 10*60*1000);
+
+var main = function() {
+  if (sms_send == 2) { process.exit(); }
+  check_price_for('2016-11-25', 'FRLLE', 'FRAEG');
+  check_price_for('2016-11-27', 'FRAEG', 'FRLLE');
+}();
